02fc91f1197b84737df35c1bd442479ecd3311cb
Alexandre Talon <alexandre.talon@ens-lyon.fr>
2014-04-05 11:24:03 +0200

    parsage terminé

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
 
 
 using namespace std;
@@ -8,10 +11,25 @@ int nbCar, nbNode, nbArc;
 int idDep;
 int tpsMax;
 
-pair<double, double> posNodes[100000];
+class s_edge
+{
+    public:
+    int dest;
+    int tps, dist;
+
+    s_edge(){}
+    s_edge(int u, int v, int w)
+    {
+        dest = u;
+        tps = v;
+        dist = w;
+    }
+};
+
+pair<double, double> posNodes[12000];
 
 
-vector<vector<int> > graph;
+vector<s_edge> graph[12000];
 
 
 int main(void)
@@ -20,7 +38,16 @@ int main(void)
 
     for (int i = 0; i < nbNode; i++)
         cin >> posNodes[i].first >> posNodes[i].second;
+    
+    for (int i = 0; i < nbArc; i++)
+    {
+        int n1, n2, bidir, tps, size;
+        cin >> n1 >> n2 >> bidir >> tps >> size;
 
+        graph[n1].push_back(s_edge(n2, tps, size));
+        if (bidir == 2)
+            graph[n2].push_back(s_edge(n1, tps, size));
+    }
 
     return 0;
 }
